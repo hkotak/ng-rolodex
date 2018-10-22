d04c0d290370676d898a4a2691793ebb63ec62d8
Harsh Kotak <kotak_harsh@hotmail.com>
2018-10-21 22:39:17 -1000

    ✨ basic routing between pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+//~~~~~~~~ IMPORT PAGES TO ROUTE TO ~~~~~~~~//
+import { LandingComponent } from './pages/landing/landing.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+
+import { HomeComponent } from './pages/home/home.component'
+
+
+const routes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' } //What does this one do?
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './app-header/header.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LandingHeaderComponent } from './landing-header/landing-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    RegisterComponent,
+    LandingComponent,
+    HomeComponent,
+    LandingHeaderComponent
   ],
   imports: [
     BrowserModule,
